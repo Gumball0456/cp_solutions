5e437f16694f6193ac8a1efd133119d4bcfefbaf
gumball <dariltonmario14@gmail.com>
2025-04-09 10:06:34 +0100

    Fix incomplete solution for codeforces problem B from contest 714

--- codeforces/714_B.cpp
+++ codeforces/714_B.cpp
@@ -5,14 +5,20 @@ using namespace std;
 
 int main(){
     int n; cin >> n;
-    int els[n];
-    for(int &el: els) cin >> el;
-    sort(els, els + n);
-    bool good = true;
-    if(n <= 3){
-
+    set<int> els;
+    int el;
+    for(int i = 0; i < n; i++) cin >> el, els.insert(el);
+    if(els.size() > 3){
+        cout << "NO";
+        return 0;
     }
-    if(good) cout << "YES";
-    else cout << "NO";
+    if(els.size() < 3){
+        cout << "YES";
+        return 0;
+    }
+    vector<int> aux;
+    for(auto &e: els) aux.push_back(e);
+    sort(aux.begin(), aux.end());
+    cout << (aux.back() - aux[1] == aux[1] - aux.front() ? "YES" : "NO");
     return 0;
 }
